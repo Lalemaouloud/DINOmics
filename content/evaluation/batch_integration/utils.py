7f7f2ae9d2bc8aee26cdbff9d51b292652e4116d
lalemaoulouds <leylamaouloud@gmail.com>
2026-05-25 03:41:42 +0000

    first commit for the downstream tasks organisation

---
+++ content/evaluation/batch_integration/utils.py
@@ -0,0 +1,26 @@
+"""
+DINOmics Evaluation Utilities
+================================
+
+Shared utility functions adapted from:
+- scGPT: https://github.com/bowang-lab/scGPT
+- scFoundation: https://github.com/biomap-research/scFoundation  
+- Geneformer: https://github.com/jessimeiler/geneformer
+- Nicheformer: https://github.com/theislab/nicheformer
+- scIB: https://github.com/theislab/scib
+- Benchmark: https://github.com/microsoft/zero-shot-scfoundation.git
+
+"""
+
+
+#### Batch Integration Utilities ####
+
+
+
+
+
+
+
+
+
+#### Zero-shot transfer Utilities ####
